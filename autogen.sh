f7578939ef8b720beab19ccfa5324b61eeff5482
Mladen Turk <mturk@jboss.org>
2019-02-01 11:13:15 +0100

    Make the script more readable

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-#rm -rf autom4te.cache
-#automake --add-missing --copy --foreign
-#autoreconf --install
-#autoheader
-
-rm -rf autom4te.cache
+rm -rf autom4te.cache >/dev/null 2>&1
 rm -f aclocal.m4
-case `uname` in Darwin*) glibtoolize --force --copy ;;
-  *) libtoolize --force --copy ;; esac
+case `uname` in
+    Darwin*)
+        glibtoolize --force --copy
+    ;;
+    *)
+        libtoolize --force --copy
+    ;;
+esac
 autoreconf --install
 automake --add-missing --foreign --copy --force-missing
 autoconf --force
 rm -rf autom4te.cache
-
